aa92a20287fff67eb5e16df06e898faf9a4348b1
helcv <vladimirhelc@gmail.com>
2024-02-12 19:33:22 +0100

    Add entities

---
+++ Bank_Data/User.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+
+namespace Bank_Data
+{
+    public class User : TableEntity
+    {
+        public string UserId { get; set; }
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public User(string UserId)
+        {
+            this.UserId = UserId;
+            PartitionKey = "BankUser";
+            RowKey = UserId;
+        }
+        public User()
+        {
+
+        }
+    }
+}
---
+++ TechStore_Data/Product.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+
+namespace TechStore_Data
+{
+    public class Product : TableEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+        public Product(string Id)
+        {
+            this.Id = Id;
+            PartitionKey = "TechProduct";
+            RowKey = Id;
+        }
+
+        public Product()
+        {
+
+        }
+    }
+}
